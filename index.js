e01cc85637d2bfcb0e0c23a57ca73f37f144eee4
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-10 13:46:33 +0300

    feat: add finding images by word and cleaning them

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&orientation=landscape&extras=url_m&client_id=VPEGnAwflLlSf991g0qx9sRLUjQ-s9VWHXXNvoSfE4E';
+const input = document.querySelector('input')
+let url = 'https://api.unsplash.com/search/photos?query=spring&orientation=landscape&extras=url_m&client_id=VPEGnAwflLlSf991g0qx9sRLUjQ-s9VWHXXNvoSfE4E';
 const galleryContainer = document.querySelector('.gallery-container')
+let img;
+input.addEventListener('change', () => {
+    url = `https://api.unsplash.com/search/photos?query=${input.value}&orientation=landscape&extras=url_m&client_id=VPEGnAwflLlSf991g0qx9sRLUjQ-s9VWHXXNvoSfE4E`;
+
+    getData()
+})
 
 async function getData() {
     const res = await fetch(url);
     const data = await res.json();
     showData(data);
 }
-getData();
+getData()
 
 function showData(data) {
+    while (galleryContainer.firstChild) {
+        galleryContainer.removeChild(galleryContainer.firstChild)
+    }
     data.results.map((elem) => {
         let imageSrc = elem.urls.regular;
-        const img = document.createElement('img');
+        img = document.createElement('img');
         img.classList.add('gallery-img')
         img.src = `${imageSrc}`;
         img.alt = `image`;
